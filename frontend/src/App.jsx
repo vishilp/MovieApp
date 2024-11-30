e4b2349d2fcf9c0e50990471ce8a50ff69c5ee74
vishilp <vishilthe3rd@gmail.com>
2024-11-29 22:52:29 -0800

    first commit of movie card component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,11 @@ import PropTypes from "prop-types";
 function App() {
   return (
     <>
-      <Text display="whats up"/>
+    
     </>
   )
 }
 
-function Text({display}){
-  return(
-    <>
-      <div>
-        <p>{display}</p>
-     </div>
-    </>
-  )
-}
 
-Text.propTypes = {
-  display: PropTypes.string.isRequired, // Declare the 'display' prop and mark it as required
-};
 
 export default App
---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+
+function MovieCard({movie}){
+
+    function onFavorite(){
+        alert("clicked")
+    }
+
+return(
+    <div className="movie-card">
+        <div className="movie-poster">
+            <img src={movie.url} alt ={movie.title}/>
+            <div className="movie-overlay">
+                <button className="favorite-button" onClick={onFavorite}> click me</button>
+            </div>
+        </div>
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{movie.release_date}</p>
+        </div>
+    </div>
+)
+}
+
+export default MovieCard
+
+MovieCard.propTypes = {
+    movie: PropTypes.string.isRequired, // Declare the 'display' prop and mark it as required
+  };
