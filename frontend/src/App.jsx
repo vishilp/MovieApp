0e74c408a264756887acd174bc8c277936852f2d
vishilp <vishilthe3rd@gmail.com>
2024-11-29 20:21:45 -0800

    worked on proptypes for react components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import PropTypes from "prop-types";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Text display="whats up"/>
     </>
   )
 }
+
+function Text({display}){
+  return(
+    <>
+      <div>
+        <p>{display}</p>
+     </div>
+    </>
+  )
+}
+
+Text.propTypes = {
+  display: PropTypes.string.isRequired, // Declare the 'display' prop and mark it as required
+};
 
 export default App
