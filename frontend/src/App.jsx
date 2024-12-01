5e77d1c94db8349b09bb17416f44a2fc6d153c39
vishilp <vishilthe3rd@gmail.com>
2024-11-30 21:23:07 -0800

    created a navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,21 @@ import './App.css'
 import Home from './pages/Home';
 import Favorites from './pages/Favorites'
 import {Routes, Route} from "react-router-dom"
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
+    <div>
+      <NavBar/>
       <main className='main-content'>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/favorites" element={<Favorites/>}/>
         </Routes>
       </main>
+
+      </div>
     </>
   );
 }
