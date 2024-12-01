b777e118bc8f23127bbcb2b552f9ba7c1b4f6824
vishilp <vishilthe3rd@gmail.com>
2024-11-30 21:50:44 -0800

    added css files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 
-import './App.css'
+import './css/App.css'
 import Home from './pages/Home';
 import Favorites from './pages/Favorites'
 import {Routes, Route} from "react-router-dom"
--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import PropTypes from "prop-types";
+import '../css/MovieCard.css'
 
 function MovieCard({movie}){
 
