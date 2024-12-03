949e9a5d2ec6a1a0f0533a78a8e9293dbb9e20b0
vishilp <vishilthe3rd@gmail.com>
2024-12-02 19:37:52 -0800

    moviecard image rendering

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -10,14 +10,14 @@ function MovieCard({movie}){
 return(
     <div className="movie-card">
         <div className="movie-poster">
-            <img src={movie.url} alt ={movie.title}/>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt ={movie.title}/>
             <div className="movie-overlay">
-                <button className="favorite-button" onClick={onFavorite}> click me</button>
+                <button className="favorite-btn" onClick={onFavorite}>❤</button>
             </div>
         </div>
         <div className="movie-info">
             <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
+            <p>{movie.release_date?.split("-")[0]}</p> {/*? to account for undefined method behavior*/}
         </div>
     </div>
 )
