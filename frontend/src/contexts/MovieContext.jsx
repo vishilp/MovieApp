92ad485a3f64aab23456bcde8f26eec2669eb544
vishilp <vishilthe3rd@gmail.com>
2024-12-04 23:01:46 -0800

    working on favorite movies react context

---
+++ frontend/src/contexts/MovieContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useContext, useEffect } from "react";
+
+const MovieContext = createContext()
+
+export const useMovieContext = () => useContext(MovieContext)
+
+export const MovieProvider = ({children}) => {
+    const [favorites, setFavorites] = useState([])
+
+    useEffect(() => {
+        const storedFavs = localStorage.getItem("favorites")
+        if (storedFavs) setFavorites(JSON.parse(storedFavs))
+    }, [])
+
+    useEffect(()=> {
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+    }, [favorites])
+
+    return <MovieContext.Provider>
+        {children}
+    </MovieContext.Provider>
+}
