e23b3da42e7914cafdcc648798a5dbaeafa462df
vishilp <vishilthe3rd@gmail.com>
2024-12-04 22:39:24 -0800

    got the search features working through an api bug

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,7 @@ export const getPopularMovies = async () => {
     return data.results
 }
 
-export const searchMovies = async () => {
+export const searchMovies = async (query) => {
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
     const data = await response.json()
     return data.results
