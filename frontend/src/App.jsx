0491b0d4a175b63928937b6e059851f95c01d576
vishilp <vishilthe3rd@gmail.com>
2024-12-05 23:27:33 -0800

    moviecontext changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,21 @@ import Home from './pages/Home';
 import Favorites from './pages/Favorites'
 import {Routes, Route} from "react-router-dom"
 import NavBar from './components/NavBar';
+import { MovieProvider } from './contexts/MovieContext';
 
 function App() {
   return (
     <>
-    <div>
-      <NavBar/>
+    <MovieProvider>
+     <NavBar/>
       <main className='main-content'>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/favorites" element={<Favorites/>}/>
         </Routes>
       </main>
+    </MovieProvider>
 
-      </div>
     </>
   );
 }
--- frontend/src/contexts/MovieContext.jsx
+++ frontend/src/contexts/MovieContext.jsx
@@ -16,7 +16,24 @@ export const MovieProvider = ({children}) => {
         localStorage.setItem('favorites', JSON.stringify(favorites))
     }, [favorites])
 
-    return <MovieContext.Provider>
+    const addToFavorites = (movie)=>{
+        setFavorites(prev => [...prev, movie])
+    }
+
+    const removeFromFavorites = (movieId) => {
+        setFavorites(prev => prev.filter(movie => movie.id !== movieId))
+    }
+
+    const isFavorite = (movieId) => {
+        return favorites.some(movie => movie.id === movieId)
+    }
+
+    const value = {
+        favorites, addToFavorites, removeFromFavorites, isFavorite
+    }
+
+    return <MovieContext.Provider value = {value}>
         {children}
     </MovieContext.Provider>
+
 }
