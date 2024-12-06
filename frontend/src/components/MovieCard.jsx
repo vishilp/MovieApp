2c01227f6b13ace39877d4106f054621651e9e22
vishilp <vishilthe3rd@gmail.com>
2024-12-05 23:35:39 -0800

    got the favorite button to work on homepage

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
 import '../css/MovieCard.css'
+import { useMovieContext } from "../contexts/MovieContext";
 
 function MovieCard({movie}){
-
-    function onFavorite(){
-        alert("clicked")
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id)
+    function onFavorite(e){
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+            else addToFavorites(movie)
     }
 
 return(
@@ -12,7 +16,7 @@ return(
         <div className="movie-poster">
             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt ={movie.title}/>
             <div className="movie-overlay">
-                <button className="favorite-btn" onClick={onFavorite}>❤</button>
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavorite}>♥</button>
             </div>
         </div>
         <div className="movie-info">
