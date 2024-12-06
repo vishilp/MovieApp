4eb43dc5749623941a50000e07c2903f2a712710
vishilp <vishilthe3rd@gmail.com>
2024-12-05 23:42:20 -0800

    favorites tab now works

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,6 +1,21 @@
 import '../css/Favorites.css'
+import {useMovieContext} from "../contexts/MovieContext"
+import MovieCard from "../components/MovieCard"
 
 function Favorites(){
+    const {favorites} = useMovieContext()
+
+    if (favorites) {
+        return (
+            <div> 
+                <h2 className="favorites">Your Favorites</h2>
+        <div className="movies-grid">
+        {favorites.map(movie => (
+            <MovieCard movie={movie} key={movie.id}/>))}
+    </div>
+    </div>)
+    }
+
     return (
         <div className="favorites-empty">
             <h2>No Favorite Movies Yet</h2>
